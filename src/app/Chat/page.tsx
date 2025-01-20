7cff7fecf9c113e629a0d764c1d94673251d5bf3
Rishin05 <rishinp26@gmail.com>
2025-01-19 23:00:49 -0500

    added use client

--- src/app/Chat/page.tsx
+++ src/app/Chat/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect, useState } from "react";
 import { database } from "../../lib/firebase";
 import { ref as dbRef, onValue, set } from "firebase/database";
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ import { ref, onValue, off, set } from "firebase/database";
 import { useRouter } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
 
-// Define the UserData type
+
 interface UserData {
   name: string;
   schoolEmail: string;
@@ -144,7 +144,7 @@ const AboutMe = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 p-4">
-      <div className="max-w-5xl mx-auto">
+      <div className="max-w-xl mx-auto">
         <div className="flex items-center mb-6">
           <button onClick={() => router.back()} className="flex items-center text-gray-600 hover:text-gray-800">
             <ArrowLeft className="w-5 h-5 mr-2" />
